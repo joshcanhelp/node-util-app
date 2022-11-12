b47809186a85256110df5cb396e77fa1f68f8bbc
Josh Cunningham <josh@joshcanhelp.com>
2022-11-12 09:38:26 -0800

    feat: add post status to WP API call

--- routes/wp-api/index.js
+++ routes/wp-api/index.js
@@ -4,8 +4,9 @@ const router = require("express").Router();
 const { requiresAuth } = require("express-openid-connect");
 
 const { WP_BASE_URL, API_AUDIENCE, API_SCOPES } = process.env;
+const postRoute = "/wp-api";
 
-router.get("/wp-api", requiresAuth(), (req, res, next) => {
+router.get(postRoute, requiresAuth(), (req, res, next) => {
   if (!WP_BASE_URL) {
     return res.send(`Missing WP_BASE_URL env variable.`);
   }
@@ -18,20 +19,30 @@ router.get("/wp-api", requiresAuth(), (req, res, next) => {
     return res.send(`Missing API_SCOPES env variable.`);
   }
 
+  if (!req.oidc.accessToken) {
+    return res.send(`No access token. <a href="/login">Try logging in</a>`);
+  }
+
   const { host } = new URL(API_AUDIENCE);
 
   res.send(
-    `<p>Posting to ${host}
+    `<p>Posting to <code>${host}</code>
     <form method="POST">
       <p><strong>Title</strong><br><input name="title" required></p>
       <p><strong>Content</strong><br><textarea name="content"></textarea></p>
+      <p><strong>Status</strong><br>
+        <input type="radio" name="status" value="publish" id="wp_status_publish" checked />
+        <label for="wp_status_publish">Publish</label>
+        <input type="radio" name="status" value="draft" id="wp_status_draft" />
+        <label for="wp_status_draft">Draft</label>
+      </p>
       <p><input type="submit" value="Post"></p>
     </form>
     <p><a href="/">Back home</a></p>`
   );
 });
 
-router.post("/wp-api", requiresAuth(), async (req, res, next) => {
+router.post(postRoute, requiresAuth(), async (req, res, next) => {
 
   if (!req.oidc.accessToken) {
     return res.send(`No access token. <a href="/login">Try logging in</a>`);
@@ -56,14 +67,12 @@ router.post("/wp-api", requiresAuth(), async (req, res, next) => {
       }
     );
   } catch (error) {
-    return res.send(
-      `<p>Error creating the post: ${error.message}</p>`
-    );
+    return res.json(error.response?.data || error.message);
   }
 
   res.send(
     `<p>Post is published <a href="${apiResponse.data.link}" target="_blank">here</a></p>
-    <p><a href="/post-to-wp">Post another 👉</a></p>`
+    <p><a href="${postRoute}">Post another 👉</a></p>`
   );
 });
 
